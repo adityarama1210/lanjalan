b43b8f5cea484750b894921bb7c7104a89c97305
adit <adityarama1210@github.com>
2016-11-13 15:18:08 +0700

    update

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.search, name='search'),
+    #url(r'^search/(?P<query>\w+)/$', views.search, name='search'),
     #^^ coba buat dapetin query
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here. # This view usually known as the controller -> wtf
 
 def index(request):
 	return render(request, 'views/index.html', {})
+
+
+def search(request, query):
+	return HttpResponse("waodkwa") 
