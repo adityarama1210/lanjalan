3769e64c392d31d6f95abdc92b9dfb933c847881
adityarama1210 <=adityarama1210@github.com>
2016-11-23 13:39:15 +0700

    add model and migrations

--- app/models.py
+++ app/models.py
@@ -3,3 +3,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+	word = models.CharField(max_length = 50)
+	class Meta:
+		db_table = 'words'
+
+class Package(models.Model):
+	name = models.CharField(max_length = 100)
+	description = models.TextField()
+	price = models.IntegerField()
+	class Meta:
+		db_table = 'packages'
+
+class DocumentWord(models.Model):
+	doc_id = models.IntegerField(primary_key = True)
+	word = models.ForeignKey(Word, on_delete = models.CASCADE)
+	freq = models.IntegerField()
+	weight = models.DecimalField(max_digits = 3, decimal_places = 3)
+	class Meta:
+		db_table = 'document_words'
+
+class TfTable(models.Model):
+	word = models.ForeignKey(Package, on_delete = models.CASCADE)
+	tf = models.IntegerField()
+	class Meta:
+		db_table = 'tf_tables'
