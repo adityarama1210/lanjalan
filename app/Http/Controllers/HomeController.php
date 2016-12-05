f2f7afcfb61cf874c0f1ff6f1f8b75024420de92
adit <adityarama1210@github.com>
2016-12-05 11:07:10 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Package;
+use App\Word;
+use App\DocumentWord;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-    	return 'index';
+    	return view('index');
     }
     public function search(Request $request){
-    	
+    	$query = $request->input('query');
+
+    	return view('index');
     }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+<form action="{{ route('search') }}" method="GET">
+    <input type="text" name="query"/>
+    <input type="submit"/>
+</form>
