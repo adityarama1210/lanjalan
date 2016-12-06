9a40de44ab791d6f027a2ea8ae8981bd633401c5
adit <adityarama1210@github.com>
2016-12-06 14:37:24 +0700

    search done!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Package;
 use App\Word;
 use App\DocumentWord;
+use DB;
 
 class HomeController extends Controller
 {
@@ -15,17 +16,56 @@ class HomeController extends Controller
     }
     public function search(Request $request){
     	$query = $request->input('query');
-    	$arr = explode(" ", $query);
+        if(!$query){
+            return redirect()->route('index');
+        }
+    	$arr = explode(" ", strtolower($query));
     	$arr = array_count_values($arr);
+        $arr_of_query = [];
         $query_weight = 0.0;
-    	foreach($arr as $w => $freq){
-            $word = Word::where('word',$w)->first();
+        $arr_of_word = array_keys($arr);
+        $words = DB::table('words')->whereIn('word', $arr_of_word)->get();
+        $words_id = [];
+        for($i = 0 ; $i < count($words) ; $i++){
+            $words_id[$i] = $words[$i]->id;
+        }
+        $document_words = DB::table('document_words')->whereIn('word_id',$words_id)->orderBy('package_id')->get();
+        $arr_of_similarity = [];
+        if(count($document_words) > 0){
+            foreach($document_words as $document_word){
+                $w = Word::find($document_word->word_id);
+                if(array_key_exists($document_word->package_id, $arr_of_similarity)){
+                    $result = $arr_of_similarity[$document_word->package_id];
+                    $result += ($document_word->weight * $w->idf * $arr[$w->word]);
+                }
+                else{
+                    $w = Word::find($document_word->word_id);
+                    $result = ($document_word->weight * $w->idf * $arr[$w->word]);
+                }
+                $arr_of_similarity[$document_word->package_id] = $result;
+            }
+            uasort($arr_of_similarity, function($a, $b){
+                if($a == $b)
+                    return 1;
+                else
+                    return $b - $a;
+            });
+            $arr = [];
+            foreach($arr_of_similarity as $package_id => $relevance_value){
+                array_push($arr, Package::find($package_id));
+            }
+            return $arr;
+        }
+        else{
+            return redirect()->back()->with('error','No Result Found');
+        }
+    	/*foreach($arr as $w => $freq){
+
             if($word){
                 $idf = $word->idf;
-                $query_weight += $freq * $idf;
+                $arr_of_query[$w] = $freq * $idf;
             }
-    	}
-    	return $query_weight;
+    	}*/
     	//return view('index');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,6 @@
+@if(session('error'))
+	<h4>{{ session('error') }}</h4>
+@endif
 <form action="{{ route('search') }}" method="GET">
     <input type="text" name="query"/>
     <input type="submit"/>
